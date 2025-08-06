3259d8de7a515225b96d42913ee97b3f7f7aca3d
SilentStorm190 <philiptaylor8475@gmail.com>
2025-08-06 07:56:17 -0500

    Update Player Ball Hub.lua

--- Player Ball Hub.lua
+++ Player Ball Hub.lua
@@ -295,7 +295,7 @@ window:Button(">Activate NDS Hub<",function()
     -- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
     -- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
     -- SOFTWARE.
-		
+
     loadstring(game:HttpGet("https://raw.githubusercontent.com/KaterHub-Inc/NaturalDisasterSurvival/refs/heads/main/main.lua"))()
     uiClickSound:Play()
 end)
